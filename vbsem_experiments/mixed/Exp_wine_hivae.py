6e7ab67bc68e8e89824a280af3951bfff67c9277
ferjorosa <ferjorosa@gmail.com>
2020-09-29 21:59:37 +0200

    Data and results update

--- vbsem_experiments/mixed/Exp_wine_hivae.py
+++ vbsem_experiments/mixed/Exp_wine_hivae.py
@@ -1,29 +1,29 @@
-import ContinuousExperiment
+import MixedExperiment
 
 
-class Exp_mobile_ksd_2016(ContinuousExperiment.ContinuousExperiment):
+class Exp_wine_hivae(MixedExperiment.MixedExperiment):
 
     def __init__(self, _data_name):
-        ContinuousExperiment.ContinuousExperiment.__init__(self, _data_name)
+        MixedExperiment.MixedExperiment.__init__(self, _data_name)
 
     def run(self, missing_percentage, n_runs, run_log):
         print("\n------------------------------------------------------------------")
         print("------------------------------------------------------------------")
-        print("------------------------ MOBILE_KSD_2016 -------------------------")
+        print("-------------------------- WINE HIVAE ----------------------------")
         print("------------------------------------------------------------------")
         print("------------------------------------------------------------------\n")
         print("Missing percentage: " + str(missing_percentage) + "\n")
 
-        ContinuousExperiment.ContinuousExperiment.run(self, missing_percentage, n_runs, run_log)
+        MixedExperiment.MixedExperiment.run(self, missing_percentage, n_runs, run_log)
 
 
 def main():
     n_runs = 5
-    data_name = "mobile_ksd_2016"
+    data_name = "wine"
     run_log = True
-    exp = Exp_mobile_ksd_2016(data_name)
+    exp = Exp_wine_hivae(data_name)
 
-    for i in range(3, 6):
+    for i in range(1, 6):
         missing_percentage = i / 10.0
         exp.run(missing_percentage, n_runs, run_log)
 
